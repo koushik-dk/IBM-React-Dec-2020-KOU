5592026cc99153dd9ef9930ff0c9b4de74095296
koushik-dk <koushik.dkarmakar@in.ibm.com>
2020-12-03 17:25:00 +0530

    day 02

---
+++ 04-state-manager/stateManager.js
@@ -0,0 +1,38 @@
+var StateManager = (function(){
+    var _currentState = undefined,
+        _callbacks = [],
+        _reducer = null,
+        _init_action = { type : '@@INIT-ACTION'};
+
+    function getState(){
+        return _currentState
+    }
+
+    function subscribe(callback){
+        _callbacks.push(callback)
+    }
+
+    //private
+    function notifySubscribers(){
+        _callbacks.forEach(callback => callback());
+    }
+
+    function dispatch(action){
+        const newState = _reducer(_currentState, action);
+        if (newState === _currentState) return;
+        _currentState = newState;
+        notifySubscribers();
+    }
+
+    function createStore(reducer){
+        if (typeof reducer !== 'function')
+            throw new Error('Reducer is manadatory!');
+        _reducer = reducer;
+        _currentState = _reducer(_currentState, _init_action);
+        const store = { getState, subscribe, dispatch };
+        return store;
+    }
+
+    return { createStore };
+
+})();
